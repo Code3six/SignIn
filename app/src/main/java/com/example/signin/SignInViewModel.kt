4648b43667490019cb4fa9eb1b6614295eeccf49
Yimage Yussef <black.yimiy96@hotmail.com>
2022-11-06 21:44:27 +0300

    u

--- app/src/main/java/com/example/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/signin/SignInViewModel.kt
@@ -1,22 +1,14 @@
 package com.example.signin
 
-import android.app.Activity
 import android.content.Intent
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
-import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.coroutines.launch
 
 
 class SignInViewModel: ViewModel() {
